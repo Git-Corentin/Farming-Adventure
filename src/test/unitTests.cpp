0f67b1dbb99d7b84305147d9a9154fe4ebafb329
Kirabsol <148810774+Kirabsol@users.noreply.github.com>
2025-06-17 23:14:35 +0200

    reverting changes because sfml audio isn't found
    to ALL CMAKELISTS

--- src/test/unitTests.cpp
+++ src/test/unitTests.cpp
@@ -19,9 +19,30 @@ TEST(SeedLogic, BacksToWheatWhenNoSeeds) {
     EXPECT_EQ(game.getSeedReservoir().getSelectedSeed(), SeedType::WHEAT);
 }
 
+TEST(SeedLogic, PlantingUsesCorrectSeed) {
+    Game game;
+    game.getSeedReservoir().addSeed(SeedType::TREE, 1);
+    game.getSeedReservoir().setSelectedSeed(SeedType::TREE);
+
+    game.plantNextSeed();
+
+    EXPECT_EQ(game.getSeedReservoir().getSeedQuantity(SeedType::TREE), 0);
+}
+
 TEST(MoneyLogic, NoOverflowMoneyAmount) {
 	Game game;
 	game.addMoney(9223372036854775807); // Maximum positive value for uint64_t
 	game.addMoney(1); // Attempt to exceed the limit
 	ASSERT_EQ(game.getMoney(), 0); // Should not allow money to exceed uint64_t limits and sends the player back to 0$
 }
+
+/*TEST(EffectLogic, CleaningRobotResetsSoilProperly) {
+    Game game;
+	game.getSoil().degrade(500); // 50% degradation
+
+    auto effect = std::make_unique<CleaningRobot>(); // suppose it increases reward
+	game.applyEffect(effect.get());
+
+    float after = game.getSoil().getDegradationRatio;
+    EXPECT_EQ(after, 0);
+}*/
