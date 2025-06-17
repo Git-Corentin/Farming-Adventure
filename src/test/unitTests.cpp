50d6c878771fd72edc694e6fed6cadb455076f90
Kirabsol <148810774+Kirabsol@users.noreply.github.com>
2025-06-17 23:37:49 +0200

    Added Effect Tests and 1 more Seed Test
    Cleaning RObot, Expiration and PLanting Seed

--- src/test/unitTests.cpp
+++ src/test/unitTests.cpp
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
 #include "Game/Game.h"
 #include "Game/SeedReservoir.h"
+#include "Effect/Utility.h"
+#include "Effect/Malus.h"
 
 TEST(Initialization, GameStartsWithDefaultValues) {
 	Game game;
@@ -36,13 +38,36 @@ TEST(MoneyLogic, NoOverflowMoneyAmount) {
 	ASSERT_EQ(game.getMoney(), 0); // Should not allow money to exceed uint64_t limits and sends the player back to 0$
 }
 
-/*TEST(EffectLogic, CleaningRobotResetsSoilProperly) {
+TEST(EffectLogic, CleaningRobotResetsSoilProperly) {
     Game game;
 	game.getSoil().degrade(500); // 50% degradation
 
-    auto effect = std::make_unique<CleaningRobot>(); // suppose it increases reward
-	game.applyEffect(effect.get());
+	std::cout << "Soil degradation ratio before applying robot:" << game.getSoil().getDegradationRatio() << std::endl;
 
-    float after = game.getSoil().getDegradationRatio;
+    auto effect = std::make_unique<CleaningRobot>();
+	effect->applyEffect(game);
+
+    float after = game.getSoil().getDegradationRatio();
+
     EXPECT_EQ(after, 0);
-}*/
+}
+
+TEST(EffectLogic, ExpirationDoesntMakeSeedQuantityNegative) {
+	Game game;
+	
+	game.getSeedReservoir().addSeed(SeedType::ORCHID, 1);
+	game.getSeedReservoir().addSeed(SeedType::BAMBOO, 1);
+
+	auto effect = std::make_unique<Expiration>();
+	effect->applyEffect(game);
+
+	bool noNegativeSeeds = true;
+	for (int i = 0; i <= static_cast<int>(SeedType::TREE); ++i) {
+		SeedType type = static_cast<SeedType>(i);
+		if (game.getSeedReservoir().getSeedQuantity(type) < 0) {
+			noNegativeSeeds = false;
+			break;
+		}
+	}
+	EXPECT_TRUE(noNegativeSeeds);
+} //Making sure that removing 3 random seeds doen't make the quantity negative
